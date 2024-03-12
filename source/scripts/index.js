69d95f6b819ff5b96a5b1913e25ad2f69e694ab2
samuse-va <vik-sam@yandex.ru>
2024-03-12 21:34:49 +0300

    add fonts, styles and img-drafts

--- source/scripts/index.js
+++ source/scripts/index.js
@@ -1,1 +1,36 @@
-/* в этот файл добавляет скрипты*/
+/* global L:readonly */
+const map = L.map("map").setView(
+  {
+    lat: 59.96831,
+    lng: 30.31748,
+  },
+  16
+);
+
+L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
+  attribution:
+    '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors | Icons made by <a href="https://www.freepik.com" title="Freepik">Freepik</a> from <a href="https://www.flaticon.com/" title="Flaticon">www.flaticon.com</a>',
+}).addTo(map);
+
+const mainPinIcon = L.icon({
+  iconUrl: "../images/icons/map-pin.svg",
+  iconSize: [38, 50],
+  iconAnchor: [19, 52],
+});
+
+const mainPinMarker = L.marker(
+  {
+    lat: 59.96831,
+    lng: 30.31748,
+  },
+  {
+    draggable: true,
+    icon: mainPinIcon,
+  }
+);
+
+mainPinMarker.addTo(map);
+
+mainPinMarker.on("moveend", (evt) => {
+  console.log(evt.target.getLatLng());
+});
