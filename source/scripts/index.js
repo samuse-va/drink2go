d2956b50b64feb389ddfb426f2aeaa2a598fa206
samuse-va <vik-sam@yandex.ru>
2024-03-15 16:05:33 +0300

    add slider and slides

--- source/scripts/index.js
+++ source/scripts/index.js
@@ -1,4 +1,5 @@
 /* global L:readonly */
+
 const map = L.map("map").setView(
   {
     lat: 59.96831,
@@ -34,3 +35,56 @@ mainPinMarker.addTo(map);
 mainPinMarker.on("moveend", (evt) => {
   console.log(evt.target.getLatLng());
 });
+
+const buttonNext = document.querySelector(".promotion__button--next");
+const buttonPrevious = document.querySelector(".promotion__button--previous");
+const slides = Array.from(document.querySelectorAll(".slider__item"));
+const sliderButtons = Array.from(
+  document.querySelectorAll(".promotion__slider-button")
+);
+const section = document.querySelector(".promotion");
+
+const slideCount = slides.length;
+let slideIndex = 0;
+
+const setColor = (index) => {
+  return `promotion--${index + 1}`;
+};
+
+buttonPrevious.addEventListener("click", showPreviousSlide);
+buttonNext.addEventListener("click", showNextSlide);
+
+sliderButtons.forEach((button) =>
+  button.addEventListener("click", () => {
+    slideIndex = sliderButtons.indexOf(button);
+    updateSlider();
+  })
+);
+
+function showPreviousSlide() {
+  slideIndex = (slideIndex - 1 + slideCount) % slideCount;
+  updateSlider();
+}
+
+function showNextSlide() {
+  slideIndex = (slideIndex + 1) % slideCount;
+  updateSlider();
+}
+
+function updateSlider() {
+  slides.forEach((slide, index) => {
+    if (index === slideIndex) {
+      slide.style.display = "flex";
+      section.classList.add(setColor(index));
+      sliderButtons[index].classList.add("promotion__slider-button--current");
+    } else {
+      slide.style.display = "none";
+      section.classList.remove(setColor(index));
+      sliderButtons[index].classList.remove(
+        "promotion__slider-button--current"
+      );
+    }
+  });
+}
+
+updateSlider();
